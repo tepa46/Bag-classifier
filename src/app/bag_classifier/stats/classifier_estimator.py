2c4aed734bc849717fc483ca00c20970c4c3731a
Kirill Shishin <shishin_ka@mail.ru>
2025-01-25 16:39:23 +0300

    chore: Delete extra estimation

--- src/app/bag_classifier/stats/classifier_estimator.py
+++ src/app/bag_classifier/stats/classifier_estimator.py
@@ -1,6 +1,6 @@
 import logging
 
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import f1_score
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +11,6 @@ def estimate_classifier(classifier, X_test, Y_test):
     """
 
     Y_pred = classifier.classifier.predict(X_test)
-
-    accuracy = accuracy_score(Y_test, Y_pred)
-    logger.info(f"$Classifier estimation$ Accuracy: {accuracy:.4f}")
 
     f1 = f1_score(Y_test, Y_pred, average="weighted")
     print(f"Weighted F1-score: {f1:.2f}")

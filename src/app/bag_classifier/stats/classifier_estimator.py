8f725eb1e58c2f476837e7a3a5f1e7451bfaa28f
Kirill Shishin <shishin_ka@mail.ru>
2025-01-25 15:26:08 +0300

    feat: Add new estimations for classifier

--- src/app/bag_classifier/stats/classifier_estimator.py
+++ src/app/bag_classifier/stats/classifier_estimator.py
@@ -1,6 +1,6 @@
 import logging
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +14,9 @@ def estimate_classifier(classifier, X_test, Y_test):
 
     accuracy = accuracy_score(Y_test, Y_pred)
     logger.info(f"$Classifier estimation$ Accuracy: {accuracy:.4f}")
+
+    f1 = f1_score(Y_test, Y_pred, average='weighted')
+    print(f"Weighted F1-score: {f1:.2f}")
+
+    f1_per_class = f1_score(Y_test, Y_pred, average=None)
+    print(f"F1-score per class: {f1_per_class}")
